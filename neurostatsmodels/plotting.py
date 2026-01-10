20446271d5223bbb5ad8e519d3496967da258ab1
jmrfox <jfox@sdsu.edu>
2026-01-09 23:45:59 -0500

    expand itd info demo nb

--- neurostatsmodels/plotting.py
+++ neurostatsmodels/plotting.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 def plot_spike_raster(
     tsgroup,
-    trial_idx=0,
+    trial_idx=None,
     neuron_labels=None,
     figsize=None,
     title=None,
@@ -19,8 +19,9 @@ def plot_spike_raster(
     tsgroup : nap.TsGroup
         TsGroup containing spike times for all neurons. Trial epochs are
         extracted from the time_support attribute.
-    trial_idx : int, optional
-        Which trial to plot. Default is 0.
+    trial_idx : int or None, optional
+        Which trial to plot. If None and there is only one trial, plots that trial.
+        If None and there are multiple trials, defaults to 0. Default is None.
     neuron_labels : list of str, optional
         Labels for each neuron. If None, uses indices 0, 1, 2, ...
     figsize : tuple, optional
@@ -48,19 +49,32 @@ def plot_spike_raster(
     >>> pop.set_means_uniform(np.linspace(-200, 200, 100))
     >>> pop.set_sigmas(50.0)
     >>>
-    >>> # Generate spikes
+    >>> # Generate spikes with multiple trials
     >>> spikes = pop.generate_spikes(0.0, duration=1.0, n_trials=5)
-    >>>
-    >>> # Plot trial 0
     >>> fig, ax = plot_spike_raster(spikes, trial_idx=0)
+    >>>
+    >>> # Generate spikes with single trial
+    >>> spikes = pop.generate_spikes(0.0, duration=1.0, n_trials=1)
+    >>> fig, ax = plot_spike_raster(spikes)  # trial_idx not needed
     """
     # Get trial interval
     trial_epochs = tsgroup.time_support
+    n_trials = len(trial_epochs)
+
+    # Handle trial_idx
+    if trial_idx is None:
+        trial_idx = 0
+
+    if trial_idx >= n_trials:
+        raise ValueError(
+            f"trial_idx={trial_idx} but only {n_trials} trial(s) available"
+        )
+
     trial_interval = trial_epochs[trial_idx]
     trial_start = trial_interval.start[0]
     trial_end = trial_interval.end[0]
     trial_duration = trial_end - trial_start
-    
+
     n_neurons = len(tsgroup)
     neuron_indices = sorted(tsgroup.keys())
 
@@ -82,14 +96,16 @@ def plot_spike_raster(
     for plot_idx, neuron_idx in enumerate(neuron_indices):
         # Restrict spikes to this trial
         trial_spikes = tsgroup[neuron_idx].restrict(trial_interval)
-        
+
         if len(trial_spikes) > 0:
             # Convert to relative time within trial
             times = trial_spikes.t - trial_start
-            
+
             # Plot spikes as vertical lines
             for time in times:
-                ax.plot([time, time], [plot_idx - 0.4, plot_idx + 0.4], 'k-', linewidth=1)
+                ax.plot(
+                    [time, time], [plot_idx - 0.4, plot_idx + 0.4], "k-", linewidth=1
+                )
 
     # Set axis properties
     ax.set_xlim(0, trial_duration)
